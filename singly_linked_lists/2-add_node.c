5c8d90c3b4f2b97bfbfeefe492400464b33cceae
norah-aln <zylayo@gmail.com>
2025-10-11 20:40:52 +0000

    Tasks 2, 3, 4: Add node, add node end, free list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -36,3 +36,70 @@ list_t *add_node(list_t **head, const char *str)
 
 	return (new);
 }
+
+/*
+ * add_node_end - adds a new node at the end of a list
+ * @head: double pointer to the head of the list
+ * @str: string to be stored in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *last;
+	unsigned int len;
+
+	if (str == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	len = 0;
+	while (str[len])
+		len++;
+
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new;
+	return (new);
+}
+
+/*
+ * free_list - frees a singly linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
